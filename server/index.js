28b3f0197e7b84a34408ae491f4e02143c44aaa4
Pawan11sinha <sinhakrpawan11@gmail.com>
2025-12-06 05:35:19 +0530

    corschange

--- server/index.js
+++ server/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 
-
-
-const doubtRoutes =require("./routes/Doubt")
+const doubtRoutes = require("./routes/Doubt");
 const userRoutes = require("./routes/User");
 const profileRoutes = require("./routes/Profile");
 const paymentRoutes = require("./routes/Payments");
@@ -12,35 +10,43 @@ const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const {cloudinaryConnect } = require("./config/cloudinary");
+const { cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-//database connect
+
 database.connect();
-//middlewares
+
+
+const allowedOrigins = [
+  "http://localhost:5173",                
+  "study-buddy-omega-tan.vercel.app", 
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
+// ---------- Middlewares ----------
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-	cors({
-		origin:"http://localhost:5173",
-		credentials:true,
-	})
-)
 
 app.use(
-	fileUpload({
-		useTempFiles:true,
-		tempFileDir:"/tmp",
-	})
-)
-//cloudinary connection
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+
 cloudinaryConnect();
 
-//routes
+
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
@@ -48,16 +54,22 @@ app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
 app.use("/api/v1/doubt", doubtRoutes);
 
-//def route
 
 app.get("/", (req, res) => {
-	return res.json({
-		success:true,
-		message:'Your server is up and running....'
-	});
+  return res.json({
+    success: true,
+    message: "Your server is up and running....",
+  });
 });
 
-app.listen(PORT, () => {
-	console.log(`App is running at ${PORT}`)
-})
+app.get("/ping", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Server is running fine ✅",
+  });
+});
 
+
+app.listen(PORT, () => {
+  console.log(`App is running at ${PORT}`);
+});
